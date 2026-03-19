4c8985dab7881e477921995988b62adeebd9a4d0
HXsolutions <haxxcelsolutions@gmail.com>
2026-03-19 16:51:29 +0500

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -52,6 +52,6 @@ app.use('/api/social', socialRoutes);
 
 app.get('/', (req, res) => res.send('API Running'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
